739446a9f97f736dc0122d7bdd907157deff2055
MuhammedAdel6041 <mohammedadelbusnis58@gmail.com>
2024-09-30 21:51:57 +0300

    Final Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
- 
+
 import './App.css'
 
 function App() {
-   
+
 
   return (
-    <div className='flex h-screen justify-center items-center bg-red-50 '>
-      <div className='bg-red-500 text-white rounded-xl h-1/5 w-3/2 text-center flex items-center justify-center flex-col p-5  text-2xl gap-5'>
-        <h1>This is Menshawy</h1>
-        <h1 className='text-6xl'>
-          Dead Hunt Project
-        </h1>
-       
-      </div>
-    </div>
+    <>
+      <h1>Welcome</h1>
+    </>
   )
 }
 
