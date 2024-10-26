8e073d4936505cf3f7fa067eef7f4825edd0d95c
MuhammedAdel6041 <mohammedadelbusnis58@gmail.com>
2024-10-26 23:09:27 +0300

    Vite_Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['chunk-G3PMV62Z']  // Replace this with the specific dependency name if available
+  }
+});
